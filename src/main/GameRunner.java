256c68e4fc0d3143db3352b5f7ee1b68c7552ac9
ReinisMazeiks <18rmazeiks@unis.org>
2017-12-02 11:03:59 -0500

    Some sample code

---
+++ src/main/GameRunner.java
@@ -0,0 +1,13 @@
+package main;
+
+import javafx.concurrent.Task;
+
+/**
+ * Created by 18rmazeiks
+ */
+public class GameRunner extends Task {
+	@Override
+	protected Object call() throws Exception {
+		return null;
+	}
+}
---
+++ src/main/Main.java
@@ -0,0 +1,33 @@
+package main;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * The main class that creates a window
+ */
+public class Main extends Application {
+	GraphicsContext gc;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		StackPane root = new StackPane();
+
+		final Canvas canvas = new Canvas(250, 250);
+		gc = canvas.getGraphicsContext2D();
+
+		root.getChildren().add(canvas);
+
+		primaryStage.setTitle("Dino");
+
+		primaryStage.setScene(new Scene(root, 300, 275));
+		primaryStage.show();
+	}
+
+
+}
